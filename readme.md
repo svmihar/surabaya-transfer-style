dfd321fd51bd1113a17adf619b497166f52dc74b
svmihar <svmihar@gmail.com>
2019-10-12 22:18:12 +0700

    :arrow_up: upgrading dependency

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-pic sumber [here](https://img.jakpost.net/c/2018/12/03/2018_12_03_59882_1543805281._large.jpg)
-pic starry night [here](https://media-dev.overstockart.com/optimized/cache/data/product_images/VG485-1000x1000.jpg)
+1. download gigapixel starry night [here](https://upload.wikimedia.org/wikipedia/commons/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg -O starry_night_gigapixel.jpg)
+
--- starry.sh
+++ starry.sh
@@ -4,7 +4,8 @@
 # Or you can manually download the image from here: https://commons.wikimedia.org/wiki/File:Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
 
 STYLE_IMAGE=starry_night_gigapixel.jpg
-CONTENT_IMAGE=examples/inputs/hoovertowernight.jpg
+CONTENT_IMAGE=examples/inputs/surabaya.jpg
+
 
 STYLE_WEIGHT=5e2
 STYLE_SCALE=1.0
@@ -23,45 +24,6 @@ NEURAL_STYLE+=$SCRIPT
 #NEURAL_STYLE=neural-style
 
 
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT \
-  -image_size 256 \
-  -output_image out1.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -cudnn_autotune
-
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -init image -init_image out1.png \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT \
-  -image_size 512 \
-  -num_iterations 500 \
-  -output_image out2.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -cudnn_autotune
-
-$NEURAL_STYLE \
-  -content_image $CONTENT_IMAGE \
-  -style_image $STYLE_IMAGE \
-  -init image -init_image out2.png \
-  -style_scale $STYLE_SCALE \
-  -print_iter 1 \
-  -style_weight $STYLE_WEIGHT \
-  -image_size 1024 \
-  -num_iterations 200 \
-  -output_image out3.png \
-  -tv_weight 0 \
-  -gpu $GPU \
-  -backend cudnn -cudnn_autotune
 
 $NEURAL_STYLE \
   -content_image $CONTENT_IMAGE \
@@ -75,7 +37,7 @@ $NEURAL_STYLE \
   -output_image out4.png \
   -tv_weight 0 \
   -gpu $GPU \
-  -backend cudnn
+  -backend cudnn -cudnn_autotune
 
 $NEURAL_STYLE \
   -content_image $CONTENT_IMAGE \
